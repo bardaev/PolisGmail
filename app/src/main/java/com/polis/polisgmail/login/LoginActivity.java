95b68653b6c663e820da1ffda9924926354f9e3a
ya111 <ppanichkin@ya.ru>
2020-05-21 20:09:01 +0300

    created directory for login activity

--- app/src/main/java/com/polis/polisgmail/login/LoginActivity.java
+++ app/src/main/java/com/polis/polisgmail/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.polis.polisgmail;
+package com.polis.polisgmail.login;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +17,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.polis.polisgmail.R;
 import com.polis.polisgmail.listmail.ListMailsActivity;
 
 
